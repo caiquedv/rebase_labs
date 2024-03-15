c4511ed7845e2c4e8e756192eff78bfd38f0f4e7
Caique <caique.log@gmail.com>
2024-03-14 21:52:13 -0400

    Refactor/Adiciona botão de voltar a listagem

--- front/public/js/listeners.js
+++ front/public/js/listeners.js
@@ -1,63 +1,82 @@
 import fetchData from './fetchData.js';
 import buildTable from './buildTable.js';
+import { fetchDataAndBuildTable } from './main.js'
 
 const listeners = {
-	handleSearchToken: function() {
-	   document.querySelector('.search-token').addEventListener('submit', async (e) => {
-		 e.preventDefault();
-   
-		 try {
-		   const token = e.target[0].value;
-		   const dataPerToken = await fetchData(`/fetch/${token}`);
-   
-		   document.getElementById('tables-list').innerHTML = '';
-   
-		   buildTable(dataPerToken, 0);
-		 } catch (error) {
-		   console.error('Error building single table:', error);
-		 }
-	   });
+	handleSearchToken: function () {
+		document.querySelector('.search-token').addEventListener('submit', async (e) => {
+			e.preventDefault();
+			const token = e.target[0].value;
+
+			if (!token) {
+				alert('Please insert a token');
+				return;
+			}
+
+			try {
+				const dataPerToken = await fetchData(`/fetch/${token}`);
+
+				document.getElementById('tables-list').innerHTML = '';
+
+				buildTable(dataPerToken, 0);
+
+				const backButton = document.querySelector('.back-list-none')
+				backButton.classList.remove('back-list-none')
+				backButton.classList.add('back-list')
+			} catch (error) {
+				console.error('Error building single table:', error);
+			}
+		});
 	},
-   
-	handleFileChange: function() {
-	   document.getElementById('csvFile').addEventListener('change', function() {
-		 var fileName = this.files[0].name;
-		 document.getElementById('fileLabel').innerText = fileName;
-	   });
+
+	handleFileChange: function () {
+		document.getElementById('csvFile').addEventListener('change', function () {
+			var fileName = this.files[0].name;
+			document.getElementById('fileLabel').innerText = fileName;
+		});
 	},
-   
-	handleCsvUpload: function() {
-	   document.querySelector('.csv-upload').addEventListener('submit', async (e) => {
-		 e.preventDefault();
-   
-		 const fileInput = document.getElementById('csvFile');
-		 if (fileInput.files.length === 0) {
-		   alert('Please select a CSV file.');
-		   return;
-		 }
-   
-		 const file = fileInput.files[0];
-   
-		 const formData = new FormData();
-		 formData.append('csvFile', file);
-   
-		 try {
-		   const response = await fetch('/fetch/csv', {
-			 method: 'POST',
-			 body: formData
-		   });
-   
-		   if (response.ok) {
-			 const msg = await response.json();
-			 alert(msg.done);
-			 document.getElementById('fileLabel').innerText = 'Import CSV file';
-		   } else {
-			 console.error('Failed to upload CSV file:', response.statusText);
-		   }
-		 } catch (error) {
-		   console.error('Error uploading CSV file:', error);
-		 }
-	   });
+
+	handleCsvUpload: function () {
+		document.querySelector('.csv-upload').addEventListener('submit', async (e) => {
+			e.preventDefault();
+
+			const fileInput = document.getElementById('csvFile');
+			if (fileInput.files.length === 0) {
+				alert('Please select a CSV file.');
+				return;
+			}
+
+			const file = fileInput.files[0];
+
+			const formData = new FormData();
+			formData.append('csvFile', file);
+
+			try {
+				const response = await fetch('/fetch/csv', {
+					method: 'POST',
+					body: formData
+				});
+
+				if (response.ok) {
+					const msg = await response.json();
+					alert(msg.done);
+					document.getElementById('fileLabel').innerText = 'Import CSV file';
+				} else {
+					console.error('Failed to upload CSV file:', response.statusText);
+				}
+			} catch (error) {
+				console.error('Error uploading CSV file:', error);
+			}
+		});
+	},
+
+	backToList: function () {
+		const backToListButton = document.getElementById('back');
+		backToListButton.addEventListener('click', () => {
+			fetchDataAndBuildTable();
+			backToListButton.classList.remove('back-list')
+			backToListButton.classList.add('back-list-none')
+		});
 	}
 };
 
--- front/public/js/main.js
+++ front/public/js/main.js
@@ -2,12 +2,19 @@ import fetchData from './fetchData.js';
 import buildTable from './buildTable.js';
 import listeners from './listeners.js';
 
-const fetchDataAndBuildTable = async () => {
+export const fetchDataAndBuildTable = async () => {
 	try {
-		const tests = await fetchData('/fetch');
-		tests.forEach((test, idx) => {
-			buildTable(test, idx);
-		});
+		if (!window.testsCach) {
+			const tests = await fetchData('/fetch');
+			window.testsCach = tests
+			tests.forEach((test, idx) => {
+				buildTable(test, idx);
+			});
+		} else {
+			window.testsCach.forEach((test, idx) => {
+				buildTable(test, idx);
+			});
+		}
 	} catch (error) {
 		console.error('Error when building table:', error);
 	}	
@@ -18,3 +25,4 @@ fetchDataAndBuildTable();
 listeners.handleSearchToken();
 listeners.handleFileChange();
 listeners.handleCsvUpload();
+listeners.backToList();
